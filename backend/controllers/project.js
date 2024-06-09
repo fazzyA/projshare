2a0ee5793efb81b1620e905cf55257683ee5de27
fazzyA <faz.pak@gmail.com>
2024-06-09 11:57:30 +0500

    schema, create project

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -1,9 +1,14 @@
+const Project = require("../models/project");
+
 const getAllProjects = (req, res) => {
   res.send("get all project");
 };
 
-const createProject = (req, res) => {
-  res.send("createProject");
+const createProject = async (req, res) => {
+  const { title, desc, image, username, url }= req.body
+  const result = await Project.create({ title, desc, image, username, url })
+  console.log("result", result)
+  res.send(result);
 };
 
 const updateProject = (req, res) => {
